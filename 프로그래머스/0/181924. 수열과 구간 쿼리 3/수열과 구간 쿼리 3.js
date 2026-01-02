8c8d25c69f93baf2d49c3074f692f75b7d632990
Remi <sdb0504@naver.com>
2026-01-03 00:12:22 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.33 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -0,0 +1,11 @@
+function solution(arr, queries) {
+    for (let k = 0; k < queries.length; k++) {
+        const [i, j] = queries[k];
+
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    return arr;
+}
