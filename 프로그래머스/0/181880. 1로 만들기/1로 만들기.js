5736d2ff999d4fac6a4169466167529474086174
Remi <sdb0504@naver.com>
2026-02-07 00:43:31 +0900

    [level 0] Title: 1로 만들기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -1,13 +1,9 @@
 function solution(num_list) {
     var answer = 0;
-    for (let num of num_list) {
-        while (num > 1) {
-            if (num % 2 === 0) {
-                num = num / 2;
-            } else {
-                num = (num - 1) / 2;
-            }
-            answer++;
+    for(let num of num_list){
+        while(num>1){
+        num = num % 2 === 0 ? num/2 : (num-1)/2;
+        answer++
         }
     }
     return answer;
