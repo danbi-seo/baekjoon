ffb4a30a83454fa95b7dd98eb80187eee3f370d0
Remi <sdb0504@naver.com>
2026-02-06 23:48:02 +0900

    [level 0] Title: 1로 만들기, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    var answer = 0;
+    for (let num of num_list) {
+        while (num > 1) {
+            if (num % 2 === 0) {
+                num = num / 2;
+            } else {
+                num = (num - 1) / 2;
+            }
+            answer++;
+        }
+    }
+    return answer;
+}
