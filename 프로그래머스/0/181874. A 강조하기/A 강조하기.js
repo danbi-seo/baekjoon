4cf77994b56ebe8d42f779186a90c6ef75849444
Remi <sdb0504@naver.com>
2026-02-22 00:27:50 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -1,11 +1,3 @@
 function solution(myString) {
-    return [...myString]
-        .map(ch =>
-            ch === 'a'
-                ? 'A'
-                : ch !== 'A' && ch === ch.toUpperCase()
-                ? ch.toLowerCase()
-                : ch
-        )
-        .join('');
+    return myString.toLowerCase().replaceAll('a', 'A');
 }
