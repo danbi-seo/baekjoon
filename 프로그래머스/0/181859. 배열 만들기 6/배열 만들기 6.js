5ca97194460654897d6107346d5a3ed01f46617d
Remi <sdb0504@naver.com>
2026-03-05 23:37:21 +0900

    [level 0] Title: 배열 만들기 6, Time: 24.05 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -1,15 +1,13 @@
 function solution(arr) {
-    const stk = [];
-
-    for (const num of arr) {
-        if (stk.length === 0) {
-            stk.push(num);
-        } else if (stk[stk.length - 1] === num) {
+    let stk = [];
+    for(const num of arr){
+        if(stk.length === 0){
+            stk.push(num)
+        } else if(stk[stk.length -1] === num){
             stk.pop();
         } else {
-            stk.push(num);
+            stk.push(num)
         }
     }
-
     return stk.length === 0 ? [-1] : stk;
 }
