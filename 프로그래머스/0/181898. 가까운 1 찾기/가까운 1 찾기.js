2236676c856bd2459a56f0655f85f6db2f09b65e
Remi <sdb0504@naver.com>
2026-03-05 23:57:06 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,12 +1,8 @@
 function solution(arr, idx) {
-  for (let i = idx; i < arr.length; i++) {
-    if (arr[i] === 1) return i;
-  }
-  return -1;
+    for (let i = idx; i < arr.length; i++) {
+        if (arr[i] === 1) {
+            return i;
+        }
+    }
+    return -1;
 }
-
-
-
-
-
-
