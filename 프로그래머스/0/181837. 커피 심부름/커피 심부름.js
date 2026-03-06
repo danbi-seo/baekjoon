aed5ece9224a06ed1ee18cf83ee4e4d3a3bcac18
Remi <sdb0504@naver.com>
2026-03-07 00:46:04 +0900

    [level 0] Title: 커피 심부름, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -1,3 +1,3 @@
 function solution(order) {
-    return order.reduce((sum, menu) => sum + (menu.includes('latte') ? 5000 : 4500), 0);
+    return order.reduce((acc, cur) => acc + (cur.includes('latte') ? 5000 : 4500), 0);
 }
