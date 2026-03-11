d45718885bc033af4ef642c8548d2c43471f4978
Remi <sdb0504@naver.com>
2026-03-11 23:19:58 +0900

    [level 0] Title: 그림 확대, Time: 0.95 ms, Memory: 34.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.js
@@ -0,0 +1,9 @@
+function solution(picture, k) {
+    var answer = [];
+    for(let row of picture){
+        for(let i = 0; i < k; i++){
+            answer.push([...row].map(v=>v.repeat(k)).join(""))
+        }
+    }
+    return answer;
+}
