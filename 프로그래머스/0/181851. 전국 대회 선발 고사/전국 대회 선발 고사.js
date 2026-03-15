8260345acdcfc96ca72e7cd99654fdeb3b932179
Remi <sdb0504@naver.com>
2026-03-16 00:14:09 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,17 +1,17 @@
 function solution(rank, attendance) {
-    let arr = [];
+    let answer = [];
 
     for (let i = 0; i < rank.length; i++) {
         if (attendance[i]) {
-            arr.push([rank[i], i]);
+            answer.push([rank[i], i]);
         }
     }
 
-    arr.sort((a, b) => a[0] - b[0]);
+    answer.sort((a, b) => a[0] - b[0]);
 
-    let a = arr[0][1];
-    let b = arr[1][1];
-    let c = arr[2][1];
+    let a = answer[0][1];
+    let b = answer[1][1];
+    let c = answer[2][1];
 
     return 10000 * a + 100 * b + c;
 }
