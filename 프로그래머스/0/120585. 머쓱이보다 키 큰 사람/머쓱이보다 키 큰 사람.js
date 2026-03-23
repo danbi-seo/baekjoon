471e45b8b7a0c4f863d3a25f8fa2a9127acc11db
Remi <sdb0504@naver.com>
2026-03-23 23:51:29 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,3 +1,9 @@
 function solution(array, height) {
-    return array.filter(v => v > height).length;
+    let count = 0;
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > height) count++;
+    }
+    
+    return count;
 }
