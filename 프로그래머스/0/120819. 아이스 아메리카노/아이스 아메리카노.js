839b97d59272fb8b5c38bb808228345dcfae5cb7
Remi <sdb0504@naver.com>
2026-09-08 10:23:22 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.06 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,3 +1,5 @@
 function solution(money) {
-    return [Math.floor(money / 5500), money % 5500];
+    let coffee = Math.floor(money / 5500)
+    let change = money % 5500
+    return [coffee, change];
 }
