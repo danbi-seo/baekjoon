782712fc9cac4e0adfbc73f598d6d18085b8c301
Remi <sdb0504@naver.com>
2026-09-08 10:37:32 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.08 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,13 +1,6 @@
 function solution(num_list) {
-    let even_num = 0
-    let odd_num = 0
-    
-    for(let i = 0; i < num_list.length; i++){
-        if(num_list[i] % 2 === 0){
-            even_num++;
-        } else{
-            odd_num++;
-        }
-    }
-    return [even_num, odd_num]
+    return [
+        num_list.filter((num) => num % 2 === 0).length,
+        num_list.filter((num) => num % 2 === 1). length
+    ]
 }
